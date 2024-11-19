cfa018642630226ea09fe237a3715f1c5a521c44
samuelhackwill <hackmew@gmail.com>
2024-11-19 18:06:34 +0100

    Add v0 parser

--- server/main.js
+++ server/main.js
@@ -1,23 +1,27 @@
-import { Meteor } from "meteor/meteor"
-import { streamer } from "../both/streamer.js"
+import { Meteor } from 'meteor/meteor'
+import { streamer } from '../both/streamer.js'
+import './parser.js'
+import { text } from '../both/text'
 
 const description = "Playtest de Novembre '24 au Melies."
 
-WebApp.connectHandlers.use("/api/hello", (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*") // Allow all origins (use specific domains for more security)
-  res.setHeader("Content-Type", "text/plain")
+WebApp.connectHandlers.use('/api/hello', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*') // Allow all origins (use specific domains for more security)
+  res.setHeader('Content-Type', 'text/plain')
 
   res.write(description)
   res.end()
 })
 
-streamer.allowRead("all")
-streamer.allowWrite("all")
+streamer.allowRead('all')
+streamer.allowWrite('all')
 
-streamer.on("pointerMessage", function (message) {
+streamer.on('pointerMessage', function (message) {
   //eventQueue.push(message);
 })
 
 Meteor.startup(async () => {
-  console.log("nuking all clients ")
+  console.log('nuking all clients ')
+  text = parseMarkdown(Assets.absoluteFilePath('text.md'))
+  console.log(text)
 })
