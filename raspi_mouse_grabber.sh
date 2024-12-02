06de65bed7684246b85be291224eee34cc40049d
samuelhackwill <hackmew@gmail.com>
2024-12-02 20:10:08 +0100

    add --rm handle to make the container auto-destruct on reboot

--- raspi_mouse_grabber.sh
+++ raspi_mouse_grabber.sh
@@ -3,9 +3,6 @@
 # Base Docker image
 IMAGE="fn61/screen-server:20240418_1823_00ddd6ec"
 
-# Base URL of the domain (can be updated if needed)
-BASE_URL="https://tryhard.samuel.ovh"
-
 # HTTP port starts from 80 and increments for each detected device
 HTTP_PORT=80
 
@@ -16,7 +13,7 @@ for MOUSE_EVENT in /dev/input/by-id/*-event-mouse; do
         CONTAINER_NAME="screen-server-$HTTP_PORT"
 
         # Construct and run the Docker command for each mouse device
-        DOCKER_CMD="sudo docker run -d --name $CONTAINER_NAME -p $HTTP_PORT:80 --shm-size 512m --security-opt seccomp=unconfined -e \"SCREEN_1=5900,800,1280,Galaxy Tab $HTTP_PORT,$MOUSE_EVENT\" --device $MOUSE_EVENT $IMAGE"
+        DOCKER_CMD="sudo docker run --rm -d --name $CONTAINER_NAME -p $HTTP_PORT:80 --shm-size 512m --security-opt seccomp=unconfined -e \"SCREEN_1=5900,800,1280,Galaxy Tab $HTTP_PORT,$MOUSE_EVENT\" --device $MOUSE_EVENT $IMAGE"
 
         echo "Running command: $DOCKER_CMD"
         eval "$DOCKER_CMD"
